da4c722d9d737dc893115c49b02a6b56febd5562
Ana Silva (1130155) <1130155@isep.ipp.pt>
2023-10-29 00:25:40 +0100

    SG3-11 - Fix issue with buildingRepo US210

--- src/mappers/BuidingMap.ts
+++ src/mappers/BuidingMap.ts
@@ -3,6 +3,9 @@ import { Room } from '../domain/room';
 import { Floor } from '../domain/floor';
 import { Building } from '../domain/building';
 import { IBuildingDto } from '../dto/IBuildingDto';
+import { IBuildingPersistence } from '../dataschema/IBuildingPersistence';
+import { Document, Model } from "mongoose";
+import { UniqueEntityID } from '../core/domain/UniqueEntityID';
 
 export class BuildingMap extends Mapper<Building> {
   public static toPersistence(building: Building): any {
@@ -21,6 +24,12 @@ export class BuildingMap extends Mapper<Building> {
       length: building.length,
       width: building.width
     } as IBuildingDto;
+  }
+
+  public static toDomain (raw: any | Model<IBuildingPersistence & Document>): Building {
+    const buildingOrError = Building.create(raw, new UniqueEntityID(raw.domainId));
+    buildingOrError.isFailure ? console.log(buildingOrError.error) : '';
+    return buildingOrError.isSuccess ? buildingOrError.getValue() : null;
   }
 
 }
