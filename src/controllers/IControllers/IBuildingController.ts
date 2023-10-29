1f01d3ec57148e8863346e6cc3e1edaec699920e
henrique <henriquevcsilva@gmail.com>
2023-10-29 20:25:05 +0000

    building controller added updateBuilding, getBuilding and getAll functions

--- src/controllers/IControllers/IBuildingController.ts
+++ src/controllers/IControllers/IBuildingController.ts
@@ -2,6 +2,11 @@ import { Request, Response, NextFunction } from 'express';
 
 export default interface IBuildingController {
   createBuilding(req: Request, res: Response, next: NextFunction);
+  updateBuilding(req: Request, res: Response, next: NextFunction);
+  getBuilding(req: Request, res: Response, next: NextFunction);
+  getAll(req: Request, res: Response, next: NextFunction);
+  findBuildingByKey(req: Request, res: Response, next: NextFunction);
+  
   getFloorsByBuildingId(req: Request, res: Response, next: NextFunction);
   getBuildingsByMinMax(req: Request, res: Response, next: NextFunction);
   getPassagesByBuildingId(req: Request, res: Response, next: NextFunction);
