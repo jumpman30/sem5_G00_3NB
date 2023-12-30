bac3260a5aa84d8ee7989676d1be8dbfc66c40b2
Henrique Silva <henriquevcsilva@gmail.com>
2023-12-30 18:04:10 +0000

    Add ITaskRepo interface and TaskRepo implementation

---
+++ src/repos/IRepos/ITaskRepo.ts
@@ -0,0 +1,13 @@
+import { Task } from '../../domain/task/Task';
+import { TaskType } from "../../domain/task/TaskType";
+import { TaskId } from "../../domain/task/TaskId";
+
+export default interface ITaskRepo {
+  save(task: Task): Promise<{ warning?: string; task: Task; }>;
+  exists(task: TaskId | string): Promise<boolean>
+  getAllTasks(): Promise<Task[]>;
+  update(task: Task): Promise<Task>;
+  findByTaskType(taskType: TaskType | string): Promise<Task>
+  findByTaskId(taskId: TaskId | string): Promise<Task>
+  delete(task: Task): Promise<Task | null>
+}
