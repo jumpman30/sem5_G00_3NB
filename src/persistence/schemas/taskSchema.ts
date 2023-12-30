4e48b51f125b8298e16e186b8f3a195544402dec
Henrique Silva <henriquevcsilva@gmail.com>
2023-12-30 18:05:41 +0000

    Add taskSchema for persistence

---
+++ src/persistence/schemas/taskSchema.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema({
+  // Common fields for all tasks
+  user: {
+    type: String,
+    required: true,
+    index: true
+  },
+  taskType: {
+    type: String,
+    required: true,
+    index: true
+  },
+  taskStatus: {
+    type: String,
+    required: true,
+    index: true
+  }},
+  { timestamps: true }
+);
+
+const Task = mongoose.model('Task', taskSchema);
